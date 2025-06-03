7a7721300759a8182a5287aab49e5b14bf15b38c
Seokki-Kwon <rnjstjrrl12@gmail.com>
2025-06-03 23:00:27 +0900

    6주차 실습과제

---
+++ ATSOPT36/week5/week5/week5App.swift
@@ -0,0 +1,17 @@
+//
+//  week5App.swift
+//  week5
+//
+//  Created by 권석기 on 5/17/25.
+//
+
+import SwiftUI
+
+@main
+struct week5App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
